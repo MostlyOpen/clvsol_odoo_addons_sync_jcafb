4e6ee014f1bb72c3c212325707fff1d2fc120d5e
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2022-03-08 21:54:56 -0300

    [ADD] clv_employee_history_sync_jcafb (Copied from branch 14.0))

---
+++ clv_employee_history_sync_jcafb/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Employee History External Sync (for CLVhealth-JCAFB Solution)',
+    'summary': 'Employee History External Sync Module used in CLVhealth-JCAFB Solution.',
+    'version': '15.0.6.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'clv_employee_history',
+        'clv_external_sync',
+    ],
+    'data': [
+        'data/hr_employee_history_sync.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
