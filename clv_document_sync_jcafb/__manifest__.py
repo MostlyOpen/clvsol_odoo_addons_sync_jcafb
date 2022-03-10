3bd385d8d6e95c185d59de95298fb37f133005fb
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2022-03-09 21:39:09 -0300

    [ADD] clv_document_sync_jcafb (Copied from branch 14.0)

---
+++ clv_document_sync_jcafb/__manifest__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Document External Sync (for CLVhealth-JCAFB Solution)',
+    'summary': 'Document External Sync Module used in CLVhealth-JCAFB Solution.',
+    'version': '15.0.6.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'clv_document',
+        # 'clv_document_jcafb',
+        'clv_external_sync',
+    ],
+    'data': [
+        'data/document_category_sync.xml',
+        'data/document_marker_sync.xml',
+        'data/document_type_sync.xml',
+        'data/document_type_parameter_sync.xml',
+        'data/document_1_sync.xml',
+        'data/document_2_sync.xml',
+        'data/document_item_1_sync.xml',
+        'data/document_item_2_sync.xml',
+        'data/document_item_3_sync.xml',
+        'data/document_item_4_sync.xml',
+        'data/document_item_5_sync.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
