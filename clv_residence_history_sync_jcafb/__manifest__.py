e311ef27b03ffc7774ad67281237c068d946a58a
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2022-03-10 21:19:57 -0300

    [ADD] clv_residence_history_sync_jcafb (Copied from branch 14.0)

---
+++ clv_residence_history_sync_jcafb/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Residence History External Sync (for CLVhealth-JCAFB Solution)',
+    'summary': 'Residence History External Sync Module used in CLVhealth-JCAFB Solution.',
+    'version': '15.0.6.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'clv_residence_history',
+        'clv_external_sync',
+    ],
+    'data': [
+        'data/residence_history_sync.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
