c66799cc7906b582e7f8f5c82a6d38f8e7b33408
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2022-03-23 20:07:54 -0300

    [ADD] clv_person_sync_jcafb (Copied from branch 14.0)

---
+++ clv_person_sync_jcafb/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Person External Sync (for CLVhealth-JCAFB Solution)',
+    'summary': 'Person External Sync Module used in CLVhealth-JCAFB Solution.',
+    'version': '15.0.6.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'clv_person_jcafb',
+        'clv_external_sync',
+    ],
+    'data': [
+        # 'data/person_age_range.xml',
+        'data/person_age_range_sync.xml',
+        'data/person_category_sync.xml',
+        'data/person_marker_sync.xml',
+        'data/person_sync.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
