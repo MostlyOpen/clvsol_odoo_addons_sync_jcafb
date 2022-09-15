33286a811cb7d521980ffffa66f3ab5035bfb5d0
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2022-09-15 18:08:21 -0300

    [IMP] clv_person_history_sync_jcafb

--- clv_person_history_sync_jcafb/__manifest__.py
+++ clv_person_history_sync_jcafb/__manifest__.py
@@ -11,8 +11,7 @@
     'license': 'AGPL-3',
     'website': 'https://github.com/CLVsol',
     'depends': [
-        'clv_person_history',
-        'clv_external_sync',
+        'clv_person_history_sync',
     ],
     'data': [
         'data/person_history_sync.xml',
